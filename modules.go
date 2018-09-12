a0395856f76a0808f004e743fdfce5c577736339
Ryan Gaus <rsg1egoman@gmail.com>
2018-09-12 11:41:48 -0400

    code: add non-mirrored logspout-http to build
    If this prooves to be a good solution, I'll mirror it.

--- modules.go
+++ modules.go
@@ -9,4 +9,5 @@ import (
 	_ "github.com/gliderlabs/logspout/transports/tls"
 	_ "github.com/gliderlabs/logspout/transports/udp"
 	_ "github.com/micahhausler/logspout-gelf"
+	_ "github.com/raychaser/logspout-http/http"
 )
