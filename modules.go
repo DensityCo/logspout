2592024fc8e8109594771b8bc994a127b05e4c23
Gus Cost <guscost@gmail.com>
2018-09-14 15:18:41 -0400

    Http plugin (#3)
    * code: add non-mirrored logspout-http to build
    
    If this prooves to be a good solution, I'll mirror it.
    
    * chore: move to forked version of logspout-http

--- modules.go
+++ modules.go
@@ -10,4 +10,5 @@ import (
 	_ "github.com/gliderlabs/logspout/transports/tls"
 	_ "github.com/gliderlabs/logspout/transports/udp"
 	_ "github.com/micahhausler/logspout-gelf"
+	_ "github.com/densityco/logspout-http/http"
 )
