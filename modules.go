24ef23933081be65648720b7780ddf6c63f69068
Gus Cost <guscost@gmail.com>
2018-09-14 15:22:58 -0400

    import density fork of logspout-gelf

--- modules.go
+++ modules.go
@@ -8,6 +8,6 @@ import (
 	_ "github.com/gliderlabs/logspout/transports/tcp"
 	_ "github.com/gliderlabs/logspout/transports/tls"
 	_ "github.com/gliderlabs/logspout/transports/udp"
-	_ "github.com/micahhausler/logspout-gelf"
+	_ "github.com/densityco/logspout-gelf"
 	_ "github.com/densityco/logspout-http/http"
 )
